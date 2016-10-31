94b98df235edef60c781404e27273e0111fb63ba
Patrick McLaughlin <mrpatricko@gmail.com>
2016-10-31 08:43:58 -0800

    Updated number-formatter for ExpressionEngine3

---
+++ system/user/addons/number_formatter/addon.setup.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+  'author'      => 'Douglas Green, Green Interactive / Update for EE3 by Patrick McLaughlin',
+  'author_url'  => 'http://douglasgreen.com/',
+  'name'        => 'Number Formatter',
+  'description' => 'Sends a value through the PHP number_format() function, and returns a formatted number.',
+  'version'     => '3.0.0',
+  'namespace'   => 'Number_formatter'
+);
